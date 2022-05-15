d118361c4f80855e2a3358354914a2c99dfc6842
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-05-15 20:24:34 +0530

    Update script.js
    New function

--- script.js
+++ script.js
@@ -160,6 +160,8 @@ submit.addEventListener('click', function adder(){
 
 	
 });
+submit.addEventListener('click', function(event){
+  event.preventDefault()});
 //<p class="line-1 anim-typewriter">Initiating Global Search</p>
 function lod_adder(){ sorry.innerHTML += "<p>Initiating Global Search</p>";
 	sorry.classList.add("line-1 anim-typewriter");}
